45072b7ba96fef6d518f00e7c8e4ab2681112599
Wijdann0 <160085539+Wijdann0@users.noreply.github.com>
2024-10-28 21:14:40 +0700

    Add files via upload

--- pages/halamanUtama/index.vue
+++ pages/halamanUtama/index.vue
@@ -14,10 +14,21 @@
         <nuxt-link to="/laporan" class="text-decoration-none">
           <div class="card bg-laporanMingguan rounded-5 text-light mx-2"> <!-- Tambahkan mx-2 -->
             <div class="card-body d-flex justify-content-center align-items-center">
-              <h2 class="">Laporan</h2>
+              <h2 class="">Laporan Harian</h2>
             </div>
           </div>
         </nuxt-link>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-6 col-lg-4 mb-5">
+        <nuxt-link to="/laporan/bulanan/" class="text-decoration-none">
+          <div class="card bg-laporanMingguan rounded-5 text-light mx-2"> <!-- Tambahkan mx-2 -->
+            <div class="card-body d-flex justify-content-center align-items-center">
+              <h2 class="">Laporan Bulanan</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
       </div>
     </div>
     <div class="row pb-3">
